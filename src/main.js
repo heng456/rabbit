04b0ee09d4136656a854ccfbb254a9f07d88092a
origin <oerker86@gmail.com>
2024-06-17 00:29:41 +0800

    首次提交

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+
+// 引入初始化样式文件
+import '@/styles/common.scss'
+
+const app = createApp(App)
+const pinia = createPinia()
+// 注册持久化插件
+app.use(pinia)
+app.use(router)
+app.mount('#app')
+
+
